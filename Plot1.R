7d9891071544ab1e7865bad5a81734c6c5c14a21
sumeetkumar1 <sumeet.jakson@gmail.com>
2020-03-09 22:25:27 +0530

    This is the answer to the first 3 question.

---
+++ Plot1.R
@@ -0,0 +1,4 @@
+aggregatedTotalByYear <- aggregate(Emissions ~ year, NEI, sum)
+png('plot1.png')
+barplot(height=aggregatedTotalByYear$Emissions, names.arg=aggregatedTotalByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' emissions at various years'),col='Red')
+dev.off()
